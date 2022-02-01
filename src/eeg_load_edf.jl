ada7c341c516d156419d439924f7a44be65bacce
Adam Wysokiński <adam.wysokinski@tutanota.com>
2022-02-01 18:42:34 +0100

    update

--- src/eeg_load_edf.jl
+++ src/eeg_load_edf.jl
@@ -194,7 +194,7 @@ function eeg_load_edf(in_file::String, read_annotations::Bool=true, header_only:
                       :sampling_rate => sampling_rate,
                       :gain => gain)
 
-    eeg = EEG(deepcopy(eeg_header), deepcopy(eeg_time), deepcopy(eeg_signals))
+    eeg = EEG(eeg_header, eeg_time, eeg_signals)
 
     return eeg
 end
