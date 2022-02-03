4a8f384c5c06b03d0fc0816a2035696f22872fd8
Adam Wysokiński <adam.wysokinski@tutanota.com>
2022-02-03 21:25:17 +0100

    update

--- src/plots.jl
+++ src/plots.jl
@@ -789,7 +789,7 @@ Plots power spectrum density.
 - `title::String` - plot title
 - `figure::String` - name of the output figure file
 """
-function eeg_plot_psd(eeg::EEG; epoch::Int64=1, channels::Union{Nothing, Int64, Vector{Float64}, UnitRange{Int64}}=nothing, UnitRange{Int64}}=nothing, labels::Vector{String}=[""], normalize::Bool=false, average::Bool=false, frq_lim::Union{Nothing, Int64, Float64}=nothing, xlabel::String="Frequency [Hz]", ylabel::String="Power [μV^2/Hz]", title::String="PSD", figure::String="")
+function eeg_plot_psd(eeg::EEG; epoch::Int64=1, channels::Union{Nothing, Int64, Vector{Float64}, UnitRange{Int64}}=nothing, labels::Vector{String}=[""], normalize::Bool=false, average::Bool=false, frq_lim::Union{Nothing, Int64, Float64}=nothing, xlabel::String="Frequency [Hz]", ylabel::String="Power [μV^2/Hz]", title::String="PSD", figure::String="")
 
     if epoch < 1 || epoch > eeg.eeg_header[:epochs_no]
         throw(ArgumentError("Epoch index out of range."))
