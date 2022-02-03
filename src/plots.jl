89877b5c8dcaf6ce3a38b0bd8c6b168b0e04d52d
Adam Wysokiński <adam.wysokinski@tutanota.com>
2022-02-03 18:16:25 +0100

    update

--- src/plots.jl
+++ src/plots.jl
@@ -633,12 +633,12 @@ Plots power spectrum density.
 
 - `signal_powers::Vector{Float64}`
 - `signal_freqs::Vector{Float64}`
-- `frq_lim::Union{Nothing, Float64}` - x-axis limit
+- `frq_lim::Union{Nothing, Int64, Float64}` - x-axis limit
 - `xlabel::String` - x-axis label
 - `ylabel::String` - y-axis label
 - `title::String` - plot title
 """
-function signal_plot_psd(signal_powers::Vector{Float64}, signal_freqs::Vector{Float64}; frq_lim::Union{Nothing, Float64}=nothing, xlabel="Frequency [Hz]", ylabel="Power [μV^2/Hz]", title="PSD")
+function signal_plot_psd(signal_powers::Vector{Float64}, signal_freqs::Vector{Float64}; frq_lim::Union{Nothing, Int64, Float64}=nothing, xlabel="Frequency [Hz]", ylabel="Power [μV^2/Hz]", title="PSD")
 
     frq_lim === nothing && (frq_lim = signal_freqs[end])
 
@@ -667,12 +667,12 @@ Plots power spectrum density.
 - `signal::Vector{Float64}`
 - `fs::Int64` - sampling frequency
 - `normalize::Bool` - normalize the `signal` prior to calculations
-- `frq_lim::Union{Nothing, Float64}` - x-axis limit
+- `frq_lim::Union{Nothing, Int64, Float64}` - x-axis limit
 - `xlabel::String` - x-axis label
 - `ylabel::String` - y-axis label
 - `title::String` - plot title
 """
-function signal_plot_psd(signal::Vector{Float64}; fs::Int64, normalize::Bool=false, frq_lim::Union{Nothing, Float64}=nothing, xlabel="Frequency [Hz]", ylabel="Power [μV^2/Hz]", title="PSD")
+function signal_plot_psd(signal::Vector{Float64}; fs::Int64, normalize::Bool=false, frq_lim::Union{Nothing, Int64, Float64}=nothing, xlabel="Frequency [Hz]", ylabel="Power [μV^2/Hz]", title="PSD")
 
     signal_freqs, signal_powers = signal_psd(signal, fs=fs, normalize=normalize)
     normalize == true && (ylabel::String="Power [dB]")
@@ -705,13 +705,13 @@ Plots power spectrum density.
 - `fs::Int64` - sampling rate
 - `normalize::Bool` - normalize the `signal` prior to calculations
 - `average::Bool` - plots average power and 95%CI for all channels
-- `frq_lim::Union{Nothing, Float64}` - x-axis limit
+- `frq_lim::Union{Nothing, Int64, Float64}` - x-axis limit
 - `labels::Vector{String}` - channel labels vector
 - `xlabel::String` - x-axis label
 - `ylabel::String` - y-axis label
 - `title::String` - plot title
 """
-function signal_plot_psd(signal::Matrix{Float64}; fs::Int64, normalize::Bool=false, average::Bool=false, frq_lim::Union{Nothing, Float64}=nothing, labels::Vector{String}=[""], xlabel::String="Frequency [Hz]", ylabel::String="Power [μV^2/Hz]", title::String="PSD")
+function signal_plot_psd(signal::Matrix{Float64}; fs::Int64, normalize::Bool=false, average::Bool=false, frq_lim::Union{Nothing, Int64, Float64}=nothing, labels::Vector{String}=[""], xlabel::String="Frequency [Hz]", ylabel::String="Power [μV^2/Hz]", title::String="PSD")
 
     normalize == true && (ylabel="Power [dB]")
 
@@ -786,13 +786,13 @@ Plots power spectrum density.
 - `labels::Vector{String}` - channel labels vector
 - `normalize::Bool` - normalize the `signal` prior to calculations
 - `average::Bool` - plots average power and 95%CI for all channels
-- `frq_lim::Union{Nothing, Float64}` - x-axis limit
+- `frq_lim::Union{Nothing, Int64, Float64}` - x-axis limit
 - `xlabel::String` - x-axis label
 - `ylabel::String` - y-axis label
 - `title::String` - plot title
 - `figure::String` - name of the output figure file
 """
-function eeg_plot_psd(eeg::EEG; epoch::Int64=1, channels::Union{Nothing, Int64, Vector{Float64}, UnitRange{Int64}}=nothing, labels::Vector{String}=[""], normalize::Bool=false, average::Bool=false, frq_lim::Union{Nothing, Float64}=nothing, xlabel::String="Frequency [Hz]", ylabel::String="Power [μV^2/Hz]", title::String="PSD", figure::String="")
+function eeg_plot_psd(eeg::EEG; epoch::Int64=1, channels::Union{Nothing, Int64, Vector{Float64}, UnitRange{Int64}}=nothing, labels::Vector{String}=[""], normalize::Bool=false, average::Bool=false, frq_lim::Union{Nothing, Int64, Float64}=nothing, xlabel::String="Frequency [Hz]", ylabel::String="Power [μV^2/Hz]", title::String="PSD", figure::String="")
 
     if epoch < 1 || epoch > eeg.eeg_header[:epochs_no]
         throw(ArgumentError("Epoch index out of range."))
