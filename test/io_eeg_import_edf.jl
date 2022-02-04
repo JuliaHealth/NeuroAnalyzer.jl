ae5b2976f20d5cb0d83d85b8a1a67b346bff4b46
Adam Wysokiński <adam.wysokinski@tutanota.com>
2022-02-04 12:10:54 +0100

    update

--- test/io_eeg_import_edf.jl
+++ test/io_eeg_import_edf.jl
@@ -1,9 +1,9 @@
 using NeuroJ
 using Test
 
-edfh = loadfile("EDFPlusTestFile.edf")
-
 edf = eeg_import_edf("eeg-test-edf.edf")
 @test edf.eeg_header[:version] == 0
 @test edf.eeg_header[:channels_no] == 19
 @test edf.eeg_header[:channel_locations] == false
+
+true
