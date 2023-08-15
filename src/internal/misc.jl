3585fe63dbf4efacee7b19dddeb81e6fe06ef46a
Adam Wysokiński <adam.wysokinski@tutanota.com>
2023-08-15 09:09:11 +0200

    add: _deprecated() code cleanup [CI SKIP]

--- src/internal/misc.jl
+++ src/internal/misc.jl
@@ -2,6 +2,8 @@ _info(s::String) = verbose == true && @info s
 
 _warn(s::String) = verbose == true && @warn s
 
+_deprecated(s1::String, s2::String) = verbose == true && @error "Function $s1() is deprecated, please use $s2() instead."
+
 _pl(x::Union{AbstractRange, AbstractVector}) = length(collect(x)) > 1 ? "s" : ""
 
 _pl(x::Real) = x > 1 ? "s" : ""
@@ -11,19 +13,9 @@ _get_range(signal::Union{AbstractVector, AbstractArray}) = round(rng(signal), di
 
 _c(n) = collect(1:n)
 
-function _tuple_max(t::Tuple{Real, Real})
-    abs(t[1]) > abs(t[2]) && (t = (-abs(t[1]), abs(t[1])))
-    abs(t[1]) < abs(t[2]) && (t = (-abs(t[2]), abs(t[2])))
-    return t
-end
+_tuple_max(t::Tuple{Real, Real}) = abs(t[1]) > abs(t[2]) ? (-abs(t[1]), abs(t[1])) : (-abs(t[2]), abs(t[2]))
 
-function _s2v(s::Union{<:Number, Vector{<:Number}})
-    if typeof(s) <: Number
-        return [s]
-    else
-        return s
-    end
-end
+_s2v(s::Union{<:Number, Vector{<:Number}}) = typeof(s) <: Number ? [s] : s
 
 function _copy_lt2ut(m::AbstractArray)
     if ndims(m) == 2
