347cf398bae9f5ca7cbe72434b63a3f78889ed21
Adam Wysokiński <adam.wysokinski@tutanota.com>
2023-09-13 23:49:42 +0200

    fix: for FFTW use MKL provider for AMD/Intel based systems, otherwise use FFTW provider

--- src/NeuroAnalyzer.jl
+++ src/NeuroAnalyzer.jl
@@ -112,6 +112,8 @@ end
 Plots.gr_cbar_width[] = 0.01
 if occursin("amd", lowercase(Sys.cpu_info()[1].model)) || occursin("intel", lowercase(Sys.cpu_info()[1].model))
     FFTW.set_provider!("mkl")
+else
+    FFTW.set_provider!("fftw")
 end
 FFTW.set_num_threads(Sys.CPU_THREADS)
 BLAS.set_num_threads(Sys.CPU_THREADS)
