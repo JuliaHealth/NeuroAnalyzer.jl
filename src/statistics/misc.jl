3337785fbd32e17ec7fc8f515bb7fd8adf0339cf
Adam Wysokiński <adam.wysokinski@tutanota.com>
2023-11-23 09:35:20 +0100

    add: r_test()

--- src/statistics/misc.jl
+++ src/statistics/misc.jl
@@ -12,6 +12,7 @@ export cvar_median
 export ci_median
 export ci_r
 export ci2z
+export r_test
 
 """
     z_score(x)
@@ -313,7 +314,10 @@ function ci_r(r::Float64; n::Int64, ci_level::Float64=0.95)
     ci_h = z_score + z_r * ci2z(ci_level)
     ci_l = z_score - z_r * ci2z(ci_level)
 
-    return (ci_l, ci_h)
+    ci_h_idx = vsearch(ci_h, z_values)
+    ci_l_idx = vsearch(ci_l, z_values)
+
+    return (r_values[ci_l_idx], r_values[ci_h_idx])
 
 end
 
@@ -335,3 +339,37 @@ function ci2z(ci_level::Float64=0.95)
     return quantile(Distributions.Normal(0.0, 1.0), 1 - (1 - ci_level) / 2)
 
 end
+
+"""
+    r_test(r1, r2; n1, n2)
+
+Test if two correlation coefficients are significantly different.
+
+# Arguments
+
+- `r1::Float64`: correlation coefficient, group 1
+- `r2::Float64`: correlation coefficient, group 2
+- `n1::Int64`: number of observations, group 1
+- `n2::Int64`: number of observations, group 2
+
+# Returns
+
+- `z_r1r2::Float64`
+"""
+function r_test(r1::Float64, r2::Float64; n1::Int64, n2::Int64)
+
+    r_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+    z_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.121, 0.131, 0.141, 0.151, 0.161, 0.172, 0.182, 0.192, 0.203, 0.213, 0.224, 0.234, 0.245, 0.255, 0.266, 0.277, 0.288, 0.299, 0.31, 0.321, 0.332, 0.343, 0.354, 0.365, 0.377, 0.389, 0.4, 0.412, 0.424, 0.436, 0.448, 0.46, 0.472, 0.485, 0.497, 0.51, 0.523, 0.536, 0.549, 0.563, 0.577, 0.59, 0.604, 0.618, 0.633, 0.648, 0.663, 0.678, 0.693, 0.709, 0.725, 0.741, 0.758, 0.775, 0.793, 0.811, 0.829, 0.848, 0.867, 0.887, 0.908, 0.929, 0.95, 0.973, 0.996, 1.02, 1.045, 1.071, 1.099, 1.127, 1.157, 1.188, 1.221, 1.256, 1.293, 1.333, 1.376, 1.422, 1.472, 1.528, 1.589, 1.658, 1.738, 1.832, 1.946, 2.092, 2.298, 2.647]
+
+    r1_idx = vsearch(r1, r_values)
+    z1_score = z_values[r1_idx]
+
+    r2_idx = vsearch(r2, r_values)
+    z2_score = z_values[r2_idx]
+
+    z_r1r2 = (z1_score - z2_score) / sqrt((1 / (n1 - 3)) + (1 / (n2 - 3)))
+
+    return z_r1r2
+
+end
+
--- test/statistics.jl
+++ test/statistics.jl
@@ -3,22 +3,22 @@ using Test
 using DataFrames
 using GLM
 
-@info "test 1/37: hildebrand_rule()"
+@info "test 1/38: hildebrand_rule()"
 @test NeuroAnalyzer.hildebrand_rule([1, 2, 3]) == 0.0
 
-@info "test 2/37: jaccard_similarity()"
+@info "test 2/38: jaccard_similarity()"
 @test NeuroAnalyzer.jaccard_similarity(ones(3), zeros(3)) == 0.0
 
-@info "test 3/37: z_score()"
+@info "test 3/38: z_score()"
 @test NeuroAnalyzer.z_score([1, 2, 3]) == [-1.0, 0.0, 1.0]
 
-@info "test 4/37: z_score()"
+@info "test 4/38: z_score()"
 @test round(NeuroAnalyzer.k_categories(10)[1]) == 3.0
 
-@info "test 5/37: effsize()"
+@info "test 5/38: effsize()"
 @test NeuroAnalyzer.effsize([1,2,3], [2,3,4]) == (cohen = 1.0, hedges = 1.0)
 
-@info "test 6/37: infcrit()"
+@info "test 6/38: infcrit()"
 x = 1:10
 y = 1:10
 df = DataFrame(:x=>x, :y=>y)
@@ -27,106 +27,109 @@ aic, bic = NeuroAnalyzer.infcrit(m)
 @test aic == 2.0
 @test bic == 2.302585092994046
 
-@info "test 7/37: outlier_detect()"
+@info "test 7/38: outlier_detect()"
 @test NeuroAnalyzer.grubbs([1, 2, 3, 4, 5]) == false
 @test NeuroAnalyzer.outlier_detect(ones(10)) == zeros(10)
 
-@info "test 8/37: seg_mean()"
+@info "test 8/38: seg_mean()"
 @test NeuroAnalyzer.seg_mean(ones(5,5,5)) == ones(5)
 
-@info "test 9/37: seg_mean()"
+@info "test 9/38: seg_mean()"
 @test NeuroAnalyzer.seg_mean(ones(5,5,5), ones(5, 5, 5)) == (seg1=ones(5), seg2=ones(5))
 
-@info "test 10/37: cmp_test()"
+@info "test 10/38: cmp_test()"
 _, _, _, df, _ = NeuroAnalyzer.cmp_test(ones(5), zeros(5), paired=true, type=:p)
 @test df == 4
 _, p1, p2 = NeuroAnalyzer.cmp_test(ones(1000), zeros(1000), paired=false, type=:perm)
 @test p1 == 0.0
 @test p2 == 0.0
 
-@info "test 11/37: cor_test()"
+@info "test 11/38: cor_test()"
 _, _, _, _, df, _ = NeuroAnalyzer.cor_test(ones(5), zeros(5))
 @test df == 8
 
-@info "test 12/37: binom_prob()"
+@info "test 12/38: binom_prob()"
 @test round(NeuroAnalyzer.binom_prob(0.5, 6, 10), digits=2) == 0.21
 
-@info "test 13/37: binom_stat()"
+@info "test 13/38: binom_stat()"
 @test NeuroAnalyzer.binom_stat(1.0, 10) == (m = 10.0, s = 0.0)
 
-@info "test 14/37: cvar_mean()"
+@info "test 14/38: cvar_mean()"
 @test NeuroAnalyzer.cvar_mean(ones(10)) == 0.0
 
-@info "test 15/37: cvar_median()"
+@info "test 15/38: cvar_median()"
 @test NeuroAnalyzer.cvar_median(ones(10)) == 0.0
 
-@info "test 16/37: cvar()"
+@info "test 16/38: cvar()"
 @test NeuroAnalyzer.cvar(0.1, 2.0) == 5.0
 
-@info "test 17/37: meang()"
+@info "test 17/38: meang()"
 @test NeuroAnalyzer.meang(ones(5)) == 1.0
 
-@info "test 18/37: meanh()"
+@info "test 18/38: meanh()"
 @test NeuroAnalyzer.meanh(ones(5)) == 1.0
 
-@info "test 19/37: meanw()"
+@info "test 19/38: meanw()"
 @test NeuroAnalyzer.meanw(ones(5), [1,2,3,4,5]) == 1.0
 
-@info "test 20/37: effsize()"
+@info "test 20/38: effsize()"
 @test NeuroAnalyzer.effsize(0.5, 0.5) == 0.0
 
-@info "test 21/37: moe()"
+@info "test 21/38: moe()"
 @test NeuroAnalyzer.moe(100) == 0.1
 
-@info "test 22/37: rng()"
+@info "test 22/38: rng()"
 @test NeuroAnalyzer.rng(1:5) == 4
 
-@info "test 23/37: se()"
+@info "test 23/38: se()"
 @test NeuroAnalyzer.se(ones(5)) == 0.0
 
-@info "test 24/37: pred_int()"
+@info "test 24/38: pred_int()"
 @test NeuroAnalyzer.pred_int(2) == 15.56
 
-@info "test 25/37: sem_diff()"
+@info "test 25/38: sem_diff()"
 @test NeuroAnalyzer.sem_diff(ones(5), zeros(5)) == 0.0
 
-@info "test 26/37: prank()"
+@info "test 26/38: prank()"
 @test NeuroAnalyzer.round.(prank([1,2,3]), digits=1) == [0.0, 0.1, 0.2]
 
-@info "test 27/37: linreg()"
+@info "test 27/38: linreg()"
 _, _, c, _, _, _, _ = NeuroAnalyzer.linreg(ones(100), zeros(100))
 @test c == [0.0, 0.0]
 
-@info "test 28/37: dprime()"
+@info "test 28/38: dprime()"
 @test NeuroAnalyzer.dprime(0.5, 0.5) == (dprime=0.0, rb=-0.0)
 
-@info "test 29/37: norminv()"
+@info "test 29/38: norminv()"
 @test NeuroAnalyzer.norminv(0.5) == 0.0
 
-@info "test 30/37: dranks()"
+@info "test 30/38: dranks()"
 @test NeuroAnalyzer.dranks(1:4) == [1, 2, 3, 3]
 
-@info "test 31/37: res_norm()"
+@info "test 31/38: res_norm()"
 @test NeuroAnalyzer.res_norm(ones(2))[2] == [0.5]
 
-@info "test 32/37: mcc()"
+@info "test 32/38: mcc()"
 @test NeuroAnalyzer.mcc(90, 90, 10, 10) == 0.8
 
-@info "test 33/37: meanc()"
+@info "test 33/38: meanc()"
 @test NeuroAnalyzer.meanc([10, 350]) == 0.0
 @test round(NeuroAnalyzer.meanc([0.17453292519943295, 6.1086523819801535], rad=true)) == 0.0
 
-@info "test 34/37: summary()"
+@info "test 34/38: summary()"
 @test NeuroAnalyzer.summary(ones(10)) == (mm = 1.0, s = 0.0, me = 1.0, mo = 1.0)
 @test NeuroAnalyzer.summary(ones(10), ones(10)) == (mm1 = 1.0, mm2 = 1.0, s1 = 0.0, s2 = 0.0, me1 = 1.0, me2 = 1.0, mo1 = 1.0, mo2 = 1.0)
 
-@info "test 35/37: ci_median()"
+@info "test 35/38: ci_median()"
 @test ci_median(collect(1:100)) == (42, 59)
 
-@info "test 36/37: ci_r()"
-@test ci_r(0.3, n=50) == (0.02410987005888493, 0.595890129941115)
+@info "test 36/38: ci_r()"
+@test ci_r(0.3, n=50) == (0.02, 0.53)
 
-@info "test 37/37: ci2z()"
+@info "test 37/38: ci2z()"
 @test ci2z(0.95) == 1.9599639845400576
+
+@info "test 38/38: r_test()"
+@test r_test(0.3, 0.6, n1=50, n2=50) == -1.8566613853904539
 
 true
