16fa196c372defba151e6592c7f846b687386316
Adam Wysokiński <adam.wysokinski@tutanota.com>
2023-12-02 11:09:00 +0100

    add: study save() and load()

--- src/io/save_load.jl
+++ src/io/save_load.jl
@@ -4,7 +4,7 @@ export load
 """
     save(obj; file_name, overwrite)
 
-Save `obj` to `file_name` file (HDF5-based).
+Save `NeuroAnalyzer.NEURO` object to `file_name` file (HDF5-based).
 
 # Arguments
 
@@ -29,7 +29,7 @@ end
 """
     load(file_name)
 
-Load `NeuroAnalyzer.NEURO` from `file_name` file (HDF5-based).
+Load `NeuroAnalyzer.NEURO` object from `file_name` file (HDF5-based).
 
 # Arguments
 
@@ -46,6 +46,49 @@ function load(file_name::String)
     obj = JLD2.load_object(file_name)
 
     _info("Loaded: " * uppercase(obj.header.recording[:data_type]) * " ($(nchannels(obj)) × $(epoch_len(obj)) × $(nepochs(obj)); $(round(obj.time_pts[end], digits=1)) s)")
+
+    return obj
+
+end
+
+"""
+    save(obj; file_name, overwrite)
+
+Save `NeuroAnalyzer.STUDY` object to `file_name` file (HDF5-based).
+
+# Arguments
+
+- `obj::NeuroAnalyzer.STUDY`
+- `file_name::String`: name of the file to save to
+- `overwrite::Bool=false`
+"""
+function save(obj::NeuroAnalyzer.STUDY; file_name::String, overwrite::Bool=false)
+
+    @assert !(isfile(file_name) && overwrite == false) "File $file_name cannot be saved, to overwrite use overwrite=true."
+    JLD2.save_object(file_name, obj)
+
+end
+
+"""
+    load(file_name)
+
+Load `NeuroAnalyzer.STUDY` object from `file_name` file (HDF5-based).
+
+# Arguments
+
+- `file_name::String`: name of the file to load
+
+# Returns
+
+- `obj::NeuroAnalyzer.STUDY`
+"""
+function load(file_name::String)
+
+    @assert isfile(file_name) "File $file_name cannot be loaded."
+
+    obj = JLD2.load_object(file_name)
+
+    _info("Loaded study: $(obj_n(obj)) objects")
 
     return obj
 
