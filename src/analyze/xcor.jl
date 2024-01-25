b8c5a90a1e7c9f7407217ca987de6545cbefbc90
Adam Wysokiński <adam.wysokinski@tutanota.com>
2024-01-25 20:16:25 +0100

    update: xcor()

--- src/analyze/xcor.jl
+++ src/analyze/xcor.jl
@@ -52,20 +52,21 @@ end
 """
    xcor(s1, s2; l, demean)
 
-Calculate cross-correlation (a measure of similarity of two signals as a function of the displacement of one relative to the other).
+Calculate cross-correlation.
 
 # Arguments
 
 - `s1::AbstractMatrix`
 - `s2::AbstractMatrix`
-- `l::Int64=round(Int64, min(size(s1, 2), 10 * log10(size(s1, 2))))`
+- `l::Int64=round(Int64, min(size(s[1, :, 1], 1) - 1, 10 * log10(size(s[1, :, 1], 1))))`: lags range is `-l:l`
 - `demean::Bool=true`: demean signal before computing cross-correlation
+- `biased::Bool=true`: calculate biased or unbiased cross-correlation
 
 # Returns
 
 - `xc::Array{Float64, 3}`
 """
-function xcor(s1::AbstractMatrix, s2::AbstractMatrix; l::Int64=round(Int64, min(size(s1, 1), 10 * log10(size(s1, 1)))), demean::Bool=true)
+function xcor(s1::AbstractMatrix, s2::AbstractMatrix; l::Int64=round(Int64, min(size(s1, 1), 10 * log10(size(s1, 1)))), demean::Bool=true, biased::Bool=true)
 
     @assert size(s1) == size(s2) "s1 and s2 must have the same size."
 
@@ -75,7 +76,7 @@ function xcor(s1::AbstractMatrix, s2::AbstractMatrix; l::Int64=round(Int64, min(
 
     @inbounds for ep_idx in 1:ep_n
         Threads.@threads for ch_idx in 1:ch_n
-            xc[1, :, ep_idx] = @views crosscor(s1[1, :, ep_idx], s2[1, :, ep_idx], -l:l, demean=demean)
+            xc[1, :, ep_idx] = @views xcor(s1[1, :, ep_idx], s2[1, :, ep_idx], l=l, demean=demean, biased=biased)
         end
     end
 
@@ -86,20 +87,21 @@ end
 """
    xcor(s1, s2; l, demean)
 
-Calculate cross-correlation (a measure of similarity of two signals as a function of the displacement of one relative to the other).
+Calculate cross-correlation.
 
 # Arguments
 
 - `s1::AbstractArray`
 - `s2::AbstractArray`
-- `l::Int64=round(Int64, min(size(s1, 2), 10 * log10(size(s1, 2))))`
+- `l::Int64=round(Int64, min(size(s[1, :, 1], 1) - 1, 10 * log10(size(s[1, :, 1], 1))))`: lags range is `-l:l`
 - `demean::Bool=true`: demean signal before computing cross-correlation
+- `biased::Bool=true`: calculate biased or unbiased cross-correlation
 
 # Returns
 
 - `xc::Array{Float64, 3}`
 """
-function xcor(s1::AbstractArray, s2::AbstractArray; l::Int64=round(Int64, min(size(s1, 2), 10 * log10(size(s1, 2)))), demean::Bool=true)
+function xcor(s1::AbstractArray, s2::AbstractArray; l::Int64=round(Int64, min(size(s1, 2), 10 * log10(size(s1, 2)))), demean::Bool=true, biased::Bool=true)
 
     @assert size(s1) == size(s2) "s1 and s2 must have the same size."
 
@@ -110,7 +112,7 @@ function xcor(s1::AbstractArray, s2::AbstractArray; l::Int64=round(Int64, min(si
 
     @inbounds for ep_idx in 1:ep_n
         Threads.@threads for ch_idx in 1:ch_n
-            xc[ch_idx, :, ep_idx] = @views crosscor(s1[ch_idx, :, ep_idx], s2[ch_idx, :, ep_idx], -l:l, demean=demean)
+            xc[ch_idx, :, ep_idx] = @views xcor(s1[ch_idx, :, ep_idx], s2[ch_idx, :, ep_idx], l=l, demean=demean, biased=biased)
         end
     end
 
@@ -121,7 +123,7 @@ end
 """
     xcor(obj1, obj2; ch1, ch2, ep1, ep2, l, norm)
 
-Calculate cross-correlation (a measure of similarity of two signals as a function of the displacement of one relative to the other).
+Calculate cross-correlation.
 
 # Arguments
 
@@ -131,8 +133,9 @@ Calculate cross-correlation (a measure of similarity of two signals as a functio
 - `ch2::Union{Int64, Vector{Int64}, AbstractRange}=signal_channels(obj2)`: index of channels, default is all signal channels
 - `ep1::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj1))`: default use all epochs
 - `ep2::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj2))`: default use all epochs
-- `l::Real=1`: lags range is `-l:l` [s]
+- `l::Int64=round(Int64, min(size(s[1, :, 1], 1) - 1, 10 * log10(size(s[1, :, 1], 1))))`: lags range is `-l:l`
 - `demean::Bool=true`: demean signal before computing cross-correlation
+- `biased::Bool=true`: calculate biased or unbiased cross-correlation
 
 # Returns
 
@@ -140,7 +143,7 @@ Named tuple containing:
 - `xc::Array{Float64, 3}`: cross-correlation
 - `l::Vector{Float64}`: lags [s]
 """
-function xcor(obj1::NeuroAnalyzer.NEURO, obj2::NeuroAnalyzer.NEURO; ch1::Union{Int64, Vector{Int64}, AbstractRange}=signal_channels(obj1), ch2::Union{Int64, Vector{Int64}, AbstractRange}=signal_channels(obj2), ep1::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj1)), ep2::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj2)), l::Real=1, demean::Bool=true)
+function xcor(obj1::NeuroAnalyzer.NEURO, obj2::NeuroAnalyzer.NEURO; ch1::Union{Int64, Vector{Int64}, AbstractRange}=signal_channels(obj1), ch2::Union{Int64, Vector{Int64}, AbstractRange}=signal_channels(obj2), ep1::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj1)), ep2::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj2)), l::Real=1, demean::Bool=true, biased::Bool=true)
 
     @assert sr(obj1) == sr(obj2) "OBJ1 and OBJ2 must have the same sampling rate."
 
@@ -158,7 +161,7 @@ function xcor(obj1::NeuroAnalyzer.NEURO, obj2::NeuroAnalyzer.NEURO; ch1::Union{I
     @assert l <= size(obj1, 2) "l must be ≤ $(size(obj1, 2))."
     @assert l >= 0 "l must be ≥ 0."
 
-    xc = @views xcor(reshape(obj1.data[ch1, :, ep1], length(ch1), :, length(ep1)), reshape(obj2.data[ch2, :, ep2], length(ch2), :, length(ep2)), l=l, demean=demean)
+    xc = @views xcor(reshape(obj1.data[ch1, :, ep1], length(ch1), :, length(ep1)), reshape(obj2.data[ch2, :, ep2], length(ch2), :, length(ep2)), l=l, demean=demean, biased=biased)
 
     return (xc=xc, l=round.(collect(-l:l) .* (1/sr(obj1)), digits=5))
 
