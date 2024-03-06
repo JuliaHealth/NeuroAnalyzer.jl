cc802dc405059750ee0c11366f9d54d7bca37747
Adam Wysokinski <adam.wysokinski@tutanota.com>
2024-03-06 11:01:32 +0100

    add: padm()

--- src/utils/pad.jl
+++ src/utils/pad.jl
@@ -1,5 +1,6 @@
 export pad0
 export pad2
+export padm
 
 """
     pad0(x, n)
@@ -46,4 +47,42 @@ function pad2(x::Union{AbstractVector, AbstractArray})
     ndims(x) == 3 && return hcat(x, zeros(eltype(x), size(x, 1), nextpow2(size(x, 2)) - size(x, 2), size(x, 3)))
     @assert ndims(x) <= 3 "pad2() works only for 1-, 2- or 3-dimension array."
     
+end
+
+"""
+    padm(x, n)
+
+Pad vector / rows of matrix / array with mean value. Works with 1-, 2- and 3-dimensional arrays.
+
+# Arguments
+
+- `x::Union{AbstractVector, AbstractArray}`
+- `n::Int64`: number of values to add
+- `mode::Symbol=:row`: how the mean is calculated:
+    - `:all`: mean of all rows
+    - `:row`: separate mean per each row
+
+# Returns
+
+- `padm::Union{AbstractVector, AbstractArray`
+"""
+function padm(x::Union{AbstractVector, AbstractArray}, n::Int64; mode::Symbol=:all)
+
+    _check_var(mode, [:all, :row], "mode")
+    @assert n >= 0 "n must be ≥ 0."
+    @assert ndims(x) <= 3 "pad0() works only for 1-, 2- or 3-dimension array."
+    
+    if ndims(x) == 1
+        m = mean(x)
+        return vcat(x, m .* ones(eltype(x), n))
+    elseif ndims(x) == 2
+        mode === :all && (m = mean(x))
+        mode === :row && (m = mean(x, dims=2))
+        return hcat(x, m .* ones(eltype(x), size(x, 1), n))
+    elseif ndims(x) == 3
+        mode === :all && (m = mean(x))
+        mode === :row && (m = mean(x, dims=2))
+        return hcat(x, m .* ones(eltype(x), size(x, 1), n, size(x, 3)))
+    end
+
 end
--- test/utils.jl
+++ test/utils.jl
@@ -15,52 +15,52 @@ m2 = [7 6 5; 4 3 2]
 a1 = ones(2, 3, 2)
 a2 = zeros(2, 3, 2)
 
-@info "test 1/69: apply()"
+@info "test 1/70: apply()"
 @test size(apply(e10, f="mean(obj, dims=1)")) == (23, 1, 10)
 
-@info "test 2/69: l1()"
+@info "test 2/70: l1()"
 @test l1(a1, a2) == 12
 
-@info "test 3/69: l2()"
+@info "test 3/70: l2()"
 @test l2(a1, a2) == 3.4641016151377544
 
-@info "test 4/69: perm_cmp()"
+@info "test 4/70: perm_cmp()"
 z, b = perm_cmp(a1, a2)
 @test round.(z) == -2.0 .* ones(2, 3)
 @test size(b) == (2, 3)
 
-@info "test 5/69: add_component()"
+@info "test 5/70: add_component()"
 x = collect(1:10)
 e10_tmp = add_component(e10, c=:x, v=x)
 @test e10_tmp.components[:x] == x
 
-@info "test 6/69: component_type()"
+@info "test 6/70: component_type()"
 @test component_type(e10_tmp, c=:x) == Vector{Int64}
 
-@info "test 7/69: extract_component()"
+@info "test 7/70: extract_component()"
 @test extract_component(e10_tmp, c=:x) == x
 
-@info "test 8/69: delete_component()"
+@info "test 8/70: delete_component()"
 e10_tmp2 = deepcopy(e10_tmp)
 delete_component!(e10_tmp2, c=:x)
 @test e10_tmp2.components == Dict()
 
-@info "test 9/69: rename_component()"
+@info "test 9/70: rename_component()"
 rename_component!(e10_tmp, c_old=:x, c_new=:y)
 @test e10_tmp.components[:y] == 1:10
 
-@info "test 10/69: reset_components()"
+@info "test 10/70: reset_components()"
 reset_components!(e10_tmp)
 @test e10_tmp.components == Dict()
 
-@info "test 11/69: vsearch()"
+@info "test 11/70: vsearch()"
 @test vsearch(2.1, [1, 2, 3, 4]) == 2
 @test vsearch([2.1, 2.9], [1, 2, 3, 4]) == [2.0, 3.0]
 
-@info "test 12/69: trim()"
+@info "test 12/70: trim()"
 @test vsplit(1:10, 2) == [[1, 6], [2, 7], [3, 8], [4, 9], [5, 10]]
 
-@info "test 13/69: fft0()"
+@info "test 13/70: fft0()"
 x = fft0(v1)
 @test length(x) == 5
 x = fft0(v1, 10)
@@ -70,20 +70,20 @@ x = rfft0(v1)
 x = rfft0(v1, 10)
 @test length(x) == div(length(v1) + 10, 2) + 1
 
-@info "test 14/69: ifft0()"
+@info "test 14/70: ifft0()"
 x = fft0(v1, 2)
 @test round.(real.(ifft0(x, 2))) == [1.0, 2.0, 3.0, 4.0, 5.0]
 
-@info "test 15/69: fft2()"
+@info "test 15/70: fft2()"
 x = fft2(v1)
 @test length(x) == 8
 x = rfft2(v1)
 @test length(x) == 5
 
-@info "test 16/69: nextpow()"
+@info "test 16/70: nextpow()"
 @test nextpow2(5) == 8
 
-@info "test 17/69: gradient()"
+@info "test 17/70: gradient()"
 g, g_l = NeuroAnalyzer.gradient(rand(10))
 @test size(g) == (10, )
 @test size(g_l) == (10, )
@@ -94,24 +94,24 @@ g, g_l = NeuroAnalyzer.gradient(rand(10, 10, 10))
 @test size(g) == (10, 10, 10)
 @test size(g_l) == (10, 10, 10)
 
-@info "test 18/69: findpeaks()"
+@info "test 18/70: findpeaks()"
 x = rand(100)
 x[10] *= 1000
 @test 10 in findpeaks(x)
 
-@info "test 19/69: hz2rads()"
+@info "test 19/70: hz2rads()"
 @test hz2rads(1) == 2pi
 
-@info "test 20/69: rads2hz()"
+@info "test 20/70: rads2hz()"
 @test rads2hz(2pi) == 1
 
-@info "test 21/69: t2f()"
+@info "test 21/70: t2f()"
 @test t2f(1000) == 1.0
 
-@info "test 22/69: f2t()"
+@info "test 22/70: f2t()"
 @test f2t(1.0) == 1000.0
 
-@info "test 23/69: freqs()"
+@info "test 23/70: freqs()"
 f, nf = freqs(0:1/10:10)
 @test length(f) == 51
 @test nf == 5
@@ -122,7 +122,7 @@ f, nf = freqs(e10)
 @test length(f) == 1281
 @test nf == 128
 
-@info "test 24/69: generate_window()"
+@info "test 24/70: generate_window()"
 s = generate_window(:hann, 100)
 @test length(s) == 100
 s = generate_window(:bh, 100)
@@ -140,160 +140,167 @@ s = generate_window(:triangle, 100)
 s = generate_window(:exp, 100)
 @test length(s) == 100
 
-@info "test 25/69: generate_sine()"
+@info "test 25/70: generate_sine()"
 s = generate_sine(10, 1:100)
 @test length(s) == 100
 
-@info "test 26/69: generate_csine()"
+@info "test 26/70: generate_csine()"
 s = generate_csine(10, 1:100)
 @test length(s) == 100
 
-@info "test 27/69: generate_sinc()"
+@info "test 27/70: generate_sinc()"
 s = generate_sinc()
 @test length(s) == 401
 
-@info "test 28/69: generate_morlet()"
+@info "test 28/70: generate_morlet()"
 s = generate_morlet(100, 10)
 @test length(s) == 201
 
-@info "test 29/69: generate_gaussian()"
+@info "test 29/70: generate_gaussian()"
 s = generate_gaussian(100, 10)
 @test length(s) == 201
 
-@info "test 30/69: generate_noise()"
+@info "test 30/70: generate_noise()"
 s = generate_noise(100)
 @test length(s) == 100
 
-@info "test 31/69: generate_morlet_fwhm()"
+@info "test 31/70: generate_morlet_fwhm()"
 s = generate_morlet_fwhm(100, 10)
 @test length(s) == 201
 
-@info "test 32/69: sr()"
+@info "test 32/70: sr()"
 @test sr(e10) == 256
 
-@info "test 33/69: nchannels()"
+@info "test 33/70: nchannels()"
 @test nchannels(e10) == 24
 
-@info "test 34/69: nepochs()"
+@info "test 34/70: nepochs()"
 @test nepochs(e10) == 10
 
-@info "test 35/69: signal_len()"
+@info "test 35/70: signal_len()"
 @test signal_len(e10) == 25600
 
-@info "test 36/69: epoch_len()"
+@info "test 36/70: epoch_len()"
 @test epoch_len(e10) == 2560
 
-@info "test 37/69: signal_channels()"
+@info "test 37/70: signal_channels()"
 @test signal_channels("eeg", ["eeg", "eeg", "ecg", "mrk"]) == [1, 2]
 @test signal_channels(e10) == 1:23
 
-@info "test 38/69: get_channel_bytype()"
+@info "test 38/70: get_channel_bytype()"
 @test get_channel_bytype(["eeg", "ecg", "mrk"], type="eeg") == 1
 @test get_channel_bytype(e10, type="eeg") == 1:19
 
-@info "test 39/69: history()"
+@info "test 39/70: history()"
 @test NeuroAnalyzer.history(e10) isa Vector{String}
 
-@info "test 40/69: labels()"
+@info "test 40/70: labels()"
 @test length(labels(e10)) == 24
 
-@info "test 41/69: channel_cluster()"
+@info "test 41/70: channel_cluster()"
 @test channel_cluster(e10, cluster=:f1) == [1, 3, 11]
 
-@info "test 42/69: band_frq()"
+@info "test 42/70: band_frq()"
 @test band_frq(256, band=:alpha) == (8.0, 13.0)
 @test band_frq(e10, band=:alpha) == (8.0, 13.0)
 
-@info "test 43/69: m_pad0()"
+@info "test 43/70: m_pad0()"
 @test m_pad0(m1) == [1 2 3; 4 5 6; 0 0 0]
 
-@info "test 44/69: m_sortperm()"
+@info "test 44/70: m_sortperm()"
 @test m_sortperm(m1) == [1 1 1; 2 2 2]
 
-@info "test 45/69: m_sort()"
+@info "test 45/70: m_sort()"
 @test m_sort(m1, [2, 1]) == [4 5 6; 1 2 3]
 
-@info "test 46/69: m_norm()"
+@info "test 46/70: m_norm()"
 @test m_norm(m1) == [0.5 1.0 1.5; 2.0 2.5 3.0]
 
-@info "test 47/69: linspace()"
+@info "test 47/70: linspace()"
 @test linspace(1 , 10, 10) == 1:10
 
-@info "test 48/69: logspace()"
+@info "test 48/70: logspace()"
 @test logspace(1, 2, 2) == [10.0, 100.0]
 
-@info "test 49/69: cmax()"
+@info "test 49/70: cmax()"
 @test cmax([1+2im, 10+10im]) == 10+10im
 
-@info "test 50/69: cmin()"
+@info "test 50/70: cmin()"
 @test cmin([1+2im, 10+10im]) == 1+2im
 
-@info "test 51/69: tuple_order()"
+@info "test 51/70: tuple_order()"
 @test tuple_order((2, 1)) == (1, 2)
 
-@info "test 52/69: cums()"
+@info "test 52/70: cums()"
 @test cums(a1) == [1.0 2.0 3.0; 1.0 2.0 3.0;;; 1.0 2.0 3.0; 1.0 2.0 3.0]
 
-@info "test 53/69: f_nearest()"
+@info "test 53/70: f_nearest()"
 m = [(1.0, 2.0) (3.0, 4.0); (5.0, 6.0) (7.0, 8.0)]
 @test f_nearest(m, (3.2, 3.9)) == (1, 2)
 
-@info "test 54/69: view_note()"
+@info "test 54/70: view_note()"
 @test view_note(e10) == ""
 
-@info "test 55/69: add_note()"
+@info "test 55/70: add_note()"
 add_note!(e10, note="test")
 @test view_note(e10) == "test"
 
-@info "test 56/69: delete_note()"
+@info "test 56/70: delete_note()"
 delete_note!(e10)
 @test view_note(e10) == ""
 
-@info "test 57/69: pad0()"
+@info "test 57/70: pad0()"
 @test pad0(v1, 5) == [1, 2, 3, 4, 5, 0, 0, 0, 0, 0]
 @test pad0(m1, 5) == [1 2 3 0 0 0 0 0; 4 5 6 0 0 0 0 0]
 
-@info "test 58/69: pad2()"
+@info "test 58/70: pad2()"
 @test pad2(v1) == [1, 2, 3, 4, 5, 0, 0, 0]
 @test pad2(m1) == [1 2 3 0; 4 5 6 0]
 
-@info "test 59/69: phases()"
+@info "test 59/70: phases()"
 @test length(phases(e10.data[1, :, 1])) == 2560
 
-@info "test 60/69: pick()"
+@info "test 60/70: pick()"
 @test NeuroAnalyzer.pick(e10, p=[:l, :f]) == [1, 3, 11]
 
-@info "test 61/69: t2s()"
+@info "test 61/70: t2s()"
 @test t2s(1.0, 256) == 256
 @test t2s(e10, t=1.0) == 257
 
-@info "test 62/69: s2t()"
+@info "test 62/70: s2t()"
 @test s2t(2560, 256) == 10.0
 @test s2t(e10, s=256) == 1.0
 
-@info "test 63/69: get_channel_bywl()"
+@info "test 63/70: get_channel_bywl()"
 @test get_channel_bywl(n, wl=760) == 1:36
 
-@info "test 64/69: size()"
+@info "test 64/70: size()"
 @test size(e10) == (24, 2560, 10)
 
-@info "test 65/69: to_df()"
+@info "test 65/70: to_df()"
 @test to_df(eeg) isa DataFrame
 
-@info "test 66/69: chtypes()"
+@info "test 66/70: chtypes()"
 @test length(chtypes(e10)) == 24
 
-@info "test 67/69: optode_labels()"
+@info "test 67/70: optode_labels()"
 n = import_nirs(joinpath(testfiles_path, "fnirs-test-nirs.nirs"))
 @test length(optode_labels(n)) == 3
 @test length(source_labels(n)) == 1
 @test length(detector_labels(n)) == 2
 
-@info "test 68/69: delmean()"
+@info "test 68/70: delmean()"
 @test delmean(v1) == [-2, -1, 0, 1, 2]
 @test delmean(a1, dims=3) == zeros(2, 3, 2)
 
-@info "test 69/69: tavg()"
+@info "test 69/70: tavg()"
 @test tavg(a1) == ones(2, 3, 1)
+
+@info "test 70/70: padm()"
+@test padm(ones(2), 2) == ones(4)
+@test padm(ones(2, 4), 2, mode=:all) == ones(2, 6)
+@test padm(ones(2, 4), 2, mode=:row) == ones(2, 6)
+@test padm(ones(2, 4, 3), 2, mode=:all) == ones(2, 6, 3)
+@test padm(ones(2, 4, 3), 2, mode=:row) == ones(2, 6, 3)
 
 true
