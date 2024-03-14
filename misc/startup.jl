9b2270190590730e33f7ed1f2e743560094f862e
Adam Wysokinski <adam.wysokinski@tutanota.com>
2024-03-14 15:20:17 +0100

    update: startup.jl [CI SKIP]

--- misc/startup.jl
+++ misc/startup.jl
@@ -1,5 +1,11 @@
+# copy this file to ~/.julia/config
+
 @info "Loading NeuroAnalyzer"
 using Revise
 using NeuroAnalyzer
 
 ENV["JULIA_REVISE"] = "manual"
+# set this according to your system
+ENV["JULIA_NUM_THREADS"] = 24
+# set this according to your system
+ENV["JULIA_NUM_PRECOMPILE_TASKS"] = 24
