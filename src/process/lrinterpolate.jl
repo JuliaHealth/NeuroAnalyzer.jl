64f1b26824edd89d53de8058ae9757a06b2a0496
Adam Wysokinski <adam.wysokinski@tutanota.com>
2024-04-22 13:51:49 +0200

    update: use reference epochs for mlinterpolate_channel() and lrinterpolate_channel()

--- src/process/lrinterpolate.jl
+++ src/process/lrinterpolate.jl
@@ -2,7 +2,7 @@ export lrinterpolate_channel
 export lrinterpolate_channel!
 
 """
-    lrinterpolate_channel(obj; ch, ep)
+    lrinterpolate_channel(obj; ch, ep, ep_ref)
 
 Interpolate channel using linear regression.
 
@@ -11,72 +11,77 @@ Interpolate channel using linear regression.
 - `obj::NeuroAnalyzer.NEURO`
 - `ch::Int64`: channel number to interpolate
 - `ep::Int64`: epoch number(s) within to interpolate
+- `ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep)`: reference epoch(s), default is all epochs except the interpolated one
 
 # Returns
 
 - `obj_new::NeuroAnalyzer.NEURO`
 """
-function lrinterpolate_channel(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64)
+function lrinterpolate_channel(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep))
 
     channels = get_channel_bytype(obj, type=obj.header.recording[:data_type])
-    @assert length(channels) > 1 "OBJ must contain > 1 signal channel."
+    @assert length(channels) > 1 "signal must contain > 1 signal channel."
     @assert ch in channels "ch must be a signal channel; cannot interpolate non-signal channels."
-    @assert nepochs(obj) > 1 "OBJ must have > 1 epoch to train the model."
+    @assert nepochs(obj) > 1 "Training the model requires the signal to have > 1 epoch."
 
-    bad_signal = obj.data[:, :, ep]
-    good_eps = setdiff(1:nepochs(obj), ep)
-    good_chs = setdiff(channels, ch)
-    good_signal = _make_epochs(obj.data[:, :, good_eps], ep_n=1)
+    _check_epochs(obj, ep_ref)
+    @assert !(ep in ep_ref) "ep must not be in ep_rep."
+
+    signal_src = obj.data[channels, :, ep]
+    ch_ref = setdiff(channels, ch)
+    signal_ref = _make_epochs(obj.data[channels, :, ep_ref], ep_n=1)
 
     # train
-    df = @views DataFrame(hcat(good_signal[ch, :, 1], good_signal[good_chs, :, 1]'), :auto)
+    df = @views DataFrame(hcat(signal_ref[ch, :, 1], signal_ref[ch_ref, :, 1]'), :auto)
     train, test = _split(df, 0.80)
     fm = Term(:x1) ~ sum(Term.(Symbol.(names(df[!, Not(:x1)]))))
     linear_regressor = lm(fm, train)
     acc_r2 = r2(linear_regressor)
     prediction = GLM.predict(linear_regressor, test)
     accuracy_testdf = DataFrame(signal_actual = test[!, :x1], signal_predicted = prediction)
-    accuracy_testdf.error = accuracy_testdf[!, :signal_actual] 
-    accuracy_testdf[!, :signal_predicted]
+    accuracy_testdf.error = accuracy_testdf[!, :signal_actual]
+    acc_rmse = sqrt(sum((accuracy_testdf.error).^2)) / length(accuracy_testdf.error)
     acc_mae = mean(abs.(accuracy_testdf.error))
     aic, bic = infcrit(linear_regressor)
 
     _info("Accuracy report:")
     _info(" R²: $(round(acc_r2, digits=3))")
+    _info(" RMSE: $(round(acc_rmse, digits=3))")
     _info(" MAE: $(round(acc_mae, digits=3))")
     _info(" AIC: $(round(aic, digits=3))")
     _info(" BIC: $(round(bic, digits=3))")
 
     # predict
     obj_new = deepcopy(obj)
-    df = @views DataFrame(hcat(bad_signal[ch, :, ep], bad_signal[good_chs, :, ep]'), :auto)
+    df = @views DataFrame(hcat(signal_src[ch, :], signal_src[ch_ref, :]'), :auto)
     obj_new.data[ch, :, ep] = GLM.predict(linear_regressor, df)
 
     reset_components!(obj_new)
-    push!(obj_new.history, "lrinterpolate_channel(OBJ, ch=$ch, ep=$ep)")
+    push!(obj_new.history, "lrinterpolate_channel(OBJ, ch=$ch, ep=$ep, ep_ref=$ep_ref)")
 
     return obj_new
     
 end
 
 """
-    lrinterpolate_channel!(obj; ch, ep)
+    lrinterpolate_channel!(obj; ch, ep, ep_ref)
 
 Interpolate channel using linear regression.
 
 # Arguments
 
 - `obj::NeuroAnalyzer.NEURO`
-- `ch::Union{Int64, Vector{Int64}, <:AbstractRange}`: channel number to interpolate
-- `ep::Union{Int64, Vector{Int64}, <:AbstractRange}`: epoch number(s) within to interpolate
+- `ch::Int64`: channel number to interpolate
+- `ep::Int64`: epoch number(s) within to interpolate
+- `ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep)`: reference epoch(s), default is all epochs except the interpolated one
 
 # Returns
 
 - `obj_new::NeuroAnalyzer.NEURO`
 """
-function lrinterpolate_channel!(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Union{Int64, Vector{Int64}, <:AbstractRange})
+function lrinterpolate_channel!(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep))
 
-    obj_new = lrinterpolate_channel(obj, ch=ch, ep=ep)
+    obj_new = lrinterpolate_channel(obj, ch=ch, ep=ep, ep_ref=ep_ref)
     obj.data = obj_new.data
     obj.components = obj_new.components
     obj.history = obj_new.history
--- src/process/mlinterpolate.jl
+++ src/process/mlinterpolate.jl
@@ -2,7 +2,7 @@ export mlinterpolate_channel
 export mlinterpolate_channel!
 
 """
-    mlinterpolate_channel(obj; ch, ep, model)
+    mlinterpolate_channel(obj; ch, ep, ep_ref, model)
 
 Interpolate channel using a machine-learning model.
 
@@ -11,28 +11,32 @@ Interpolate channel using a machine-learning model.
 - `obj::NeuroAnalyzer.NEURO`
 - `ch::Int64`: channel number to interpolate
 - `ep::Int64`: epoch number within to interpolate
+- `ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep)`: reference epoch(s), default is all epochs except the interpolated one
 - `model<:MLJ.Model`: MLJ regressor model
 
 # Returns
 
 - `obj_new::NeuroAnalyzer.NEURO`
 """
-function mlinterpolate_channel(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, model::T) where {T <: MLJ.Model}
+function mlinterpolate_channel(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep), model::T) where {T <: MLJ.Model}
 
     channels = get_channel_bytype(obj, type=obj.header.recording[:data_type])
     @assert length(channels) > 1 "signal must contain > 1 signal channel."
     @assert ch in channels "ch must be a signal channel; cannot interpolate non-signal channels."
     @assert nepochs(obj) > 1 "Training the model requires the signal to have > 1 epoch."
 
-    good_chs = setdiff(channels, ch)
-    good_eps = setdiff(1:nepochs(obj), ep)
-    good_signal = _make_epochs(obj.data[:, :, good_eps], ep_n=1)[:, :]
+    _check_epochs(obj, ep_ref)
+    @assert !(ep in ep_ref) "ep must not be in ep_rep."
+
+    ch_ref = setdiff(channels, ch)
+    signal_ref = _make_epochs(obj.data[channels, :, ep_ref], ep_n=1)[:, :]
 
     # train
-    y = good_signal[ch, :, 1]
-    x = table(good_signal[good_chs, :, 1]')
+    y = signal_ref[ch, :, 1]
+    x = table(signal_ref[ch_ref, :, 1]')
     mach = MLJ.machine(model, x, y)
     MLJ.fit!(mach)
+    # reconstruct
     yhat = MLJ.predict(mach, x)
 
     _info("Accuracy report:")
@@ -43,18 +47,18 @@ function mlinterpolate_channel(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, m
 
     # predict
     obj_new = deepcopy(obj)
-    x = table(obj.data[good_chs, :, ep]')
+    x = table(obj.data[ch_ref, :, ep]')
     obj_new.data[ch, :, ep] = MLJ.predict(mach, x)
 
     reset_components!(obj_new)
-    push!(obj_new.history, "mlinterpolate_channel(OBJ, ch=$ch, ep=$ep, model)")
+    push!(obj_new.history, "mlinterpolate_channel(OBJ, ch=$ch, ep=$ep, ep_ref=$ep_ref, model)")
 
     return obj_new
     
 end
 
 """
-    mlinterpolate_channel!(obj; ch, ep, model)
+    mlinterpolate_channel!(obj; ch, ep, ep_ref, model)
 
 Interpolate channel using linear regression.
 
@@ -63,15 +67,16 @@ Interpolate channel using linear regression.
 - `obj::NeuroAnalyzer.NEURO`
 - `ch::Int64`: channel number to interpolate
 - `ep::Int64`: epoch number within to interpolate
+- `ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep)`: reference epoch(s), default is all epochs except the interpolated one
 - `model::T where T <: DataType`: MLJ regressor model
 
 # Returns
 
 - `obj_new::NeuroAnalyzer.NEURO`
 """
-function mlinterpolate_channel!(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, model::T) where {T <: MLJ.Model}
+function mlinterpolate_channel!(obj::NeuroAnalyzer.NEURO; ch::Int64, ep::Int64, ep_ref::Union{Int64, Vector{Int64}, AbstractRange}=setdiff(_c(nepochs(obj)), ep), model::T) where {T <: MLJ.Model}
 
-    obj_new = mlinterpolate_channel(obj, ch=ch, ep=ep, model=model)
+    obj_new = mlinterpolate_channel(obj, ch=ch, ep=ep, ep_ref=ep_ref, model=model)
     obj.data = obj_new.data
     obj.components = obj_new.components
     obj.history = obj_new.history
