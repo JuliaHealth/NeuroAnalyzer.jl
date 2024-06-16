5093adaca04898bbad89d991226c6df62edc3bec
Adam Wysokinski <adam.wysokinski@tutanota.com>
2024-06-16 12:28:25 +0200

    update

--- src/statistics/misc.jl
+++ src/statistics/misc.jl
@@ -1,10 +1,8 @@
 export z_score
 export k_categories
-export se
+export sem
 export rng
 export moe
-export cvar
-export effsize
 export binom_prob
 export binom_stat
 export cvar_mean
@@ -12,13 +10,13 @@ export cvar_median
 export ci_median
 export ci_prop
 export ci_r
-export r_test
+export r1r2_test
 export slope
 export distance
 export count_thresh
 export crit_t
 export crit_z
-export z2pow
+export z2p
 export cmp_stat
 export fwhm
 export cosine_similarity
@@ -68,7 +66,7 @@ function k_categories(n::Int64)
 end
 
 """
-    se(x)
+    sem(x)
 
 Calculate standard error of the mean.
 
@@ -78,11 +76,13 @@ Calculate standard error of the mean.
 
 # Returns
 
-- `se::Float64`
+- `s::Float64`
 """
-function se(x::AbstractVector)
+function sem(x::AbstractVector)
 
-    return std(x) / sqrt(length(x))
+    s = std(x) / sqrt(length(x))
+
+    return s
 
 end
 
@@ -97,11 +97,13 @@ Calculate range.
 
 # Returns
 
-- `rng::Float64`
+- `r::Float64`
 """
 function rng(x::AbstractArray)
 
-    return maximum(x) - minimum(x)
+    r = maximum(x) - minimum(x)
+
+    return r
 
 end
 
@@ -116,55 +118,13 @@ Calculate margin of error for given sample size `n`.
 
 # Returns
 
-- `moe::Float64`
+- `m::Float64`
 """
 function moe(n::Int64)
 
-    return 1 / sqrt(n)
+    m = 1 / sqrt(n)
 
-end
-
-"""
-    cvar(se, s)
-
-Calculate coefficient of variation for statistic `s`.
-
-# Arguments
-
-- `se::Real`: standard error
-- `s::Real`: statistics, e.g. mean value
-
-# Returns
-
-- `cvar::Float64`
-"""
-function cvar(se::Real, s::Real)
-
-    return 100 * (se / s)
-
-end
-
-"""
-    effsize(p1, p2)
-
-Calculate effect size for two proportions `p1` and `p2`.
-
-# Arguments
-
-- `p1::Float64`: 1st proportion, e.g. 0.7
-- `p2::Float64`: 2nd proportion, e.g. 0.3
-
-# Returns
-
-- `e::Float64`
-"""
-function effsize(p1::Float64, p2::Float64)
-
-    @assert p1 + p2 == 1.0 "Proportions must add to 1.0."
-
-    e = 2 * asin(sqrt(p1)) - 2 * asin(sqrt(p2))
-
-    return e
+    return m
 
 end
 
@@ -311,7 +271,7 @@ function ci_median(x::AbstractArray; ci::Float64=0.95)
 end
 
 """
-    ci_prop(p; ci)
+    ci_prop(p, n; ci)
 
 Calculate confidence interval for a proportion.
 
@@ -383,7 +343,7 @@ Calculate confidence interval for a correlation coefficient.
 
 # Arguments
 
-- `r::Float64`
+- `r::Float64`: correlation coefficient
 - `n::Int64`: number of observations
 - `ci::Float64=0.95`: confidence level
 
@@ -412,7 +372,7 @@ function ci_r(; r::Float64, n::Int64, ci::Float64=0.95)
 end
 
 """
-    r_test(; r1, r2, n1, n2)
+    r1r2_test(; r1, r2, n1, n2)
 
 Test if two correlation coefficients are significantly different.
 
@@ -425,9 +385,9 @@ Test if two correlation coefficients are significantly different.
 
 # Returns
 
-- `z_r1r2::Float64`
+- `z::Float64`: z score
 """
-function r_test(; r1::Float64, r2::Float64, n1::Int64, n2::Int64)
+function r1r2_test(; r1::Float64, r2::Float64, n1::Int64, n2::Int64)
 
     r_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
     z_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.121, 0.131, 0.141, 0.151, 0.161, 0.172, 0.182, 0.192, 0.203, 0.213, 0.224, 0.234, 0.245, 0.255, 0.266, 0.277, 0.288, 0.299, 0.31, 0.321, 0.332, 0.343, 0.354, 0.365, 0.377, 0.389, 0.4, 0.412, 0.424, 0.436, 0.448, 0.46, 0.472, 0.485, 0.497, 0.51, 0.523, 0.536, 0.549, 0.563, 0.577, 0.59, 0.604, 0.618, 0.633, 0.648, 0.663, 0.678, 0.693, 0.709, 0.725, 0.741, 0.758, 0.775, 0.793, 0.811, 0.829, 0.848, 0.867, 0.887, 0.908, 0.929, 0.95, 0.973, 0.996, 1.02, 1.045, 1.071, 1.099, 1.127, 1.157, 1.188, 1.221, 1.256, 1.293, 1.333, 1.376, 1.422, 1.472, 1.528, 1.589, 1.658, 1.738, 1.832, 1.946, 2.092, 2.298, 2.647]
@@ -438,14 +398,55 @@ function r_test(; r1::Float64, r2::Float64, n1::Int64, n2::Int64)
     r2_idx = vsearch(r2, r_values)
     z2_score = z_values[r2_idx]
 
-    z_r1r2 = (z1_score - z2_score) / sqrt((1 / (n1 - 3)) + (1 / (n2 - 3)))
+    z = (z1_score - z2_score) / sqrt((1 / (n1 - 3)) + (1 / (n2 - 3)))
 
-    return z_r1r2
+    return z
 
 end
 
-slope(p1::Tuple{Real, Real}, p2::Tuple{Real, Real}) = (p2[2] - p1[2]) / (p2[1] - p1[1])
-distance(p1::Tuple{Real, Real}, p2::Tuple{Real, Real}) = sqrt((p2[1] - p1[1])^2 + (p2[2] - p1[2])^2)
+"""
+    slope(p1, p2)
+
+Calculate slope of the line crossing two points.
+
+# Arguments
+
+- `p1::Tuple{Real, Real}`
+- `p2::Tuple{Real, Real}`
+
+# Returns
+
+- `s::Float64`: slope
+"""
+function slope(p1::Tuple{Real, Real}, p2::Tuple{Real, Real})
+
+    s = (p2[2] - p1[2]) / (p2[1] - p1[1])
+
+    return s
+
+end
+
+"""
+    slope(p1, p2)
+
+Calculate distance between two points.
+
+# Arguments
+
+- `p1::Tuple{Real, Real}`
+- `p2::Tuple{Real, Real}`
+
+# Returns
+
+- `d::Float64`: distance
+"""
+function distance(p1::Tuple{Real, Real}, p2::Tuple{Real, Real})
+
+    d = sqrt((p2[1] - p1[1])^2 + (p2[2] - p1[2])^2)
+
+    return d
+
+end
 
 """
     count_thresh(x; t, t_type)
@@ -456,7 +457,12 @@ Collect thresholded elements, e.g. in a topographical map.
 
 - `x::AbstractMatrix`
 - `t::Real`: threshold value
-- `t_type::Symbol=:g`: rule for thresholding: = (`:eq`), ≥ (`:geq`), ≤ (`:leq`), > (`:g`), < (`:l`)
+- `t_type::Symbol=:g`: rule for thresholding:
+    - `:eq`: =
+    - `:geq`: ≥
+    - `:leq`: ≤
+    - `:g`: >
+    - `:l`: <
 
 # Returns
 
@@ -468,21 +474,21 @@ function count_thresh(x::AbstractMatrix; t::Real, t_type::Symbol=:g)
 
     _check_var(t_type, [:eq, :geq, :leq, :g, :l], "t_type")
 
-    x_t = zeros(Int64, size(x))
+    x_t = zeros(Bool, size(x))
 
     if t_type === :eq
-        x_t[x .== t] .= 1
+        x_t[x .== t] .= true
     elseif t_type === :g
-        x_t[x .> t] .= 1
+        x_t[x .> t] .= true
     elseif t_type === :geq
-        x_t[x .>= t] .= 1
+        x_t[x .>= t] .= true
     elseif t_type === :l
-        x_t[x .< t] .= 1
+        x_t[x .< t] .= true
     elseif t_type === :leq
-        x_t[x .<= t] .= 1
+        x_t[x .<= t] .= true
     end
 
-    n = count(==(1), x_t)
+    n = count(==(true), x_t)
 
     return (x_t=x_t, n=n)
 
@@ -523,7 +529,7 @@ Calculate critical z value.
 
 - `z::Float64`
 """
-function crit_z(c::Float64=0.95; twosided::Bool=true)
+function crit_z(c::Float64=0.95)
 
     z = quantile(Distributions.Normal(0.0, 1.0), 1 - (1 - c) / 2)
 
@@ -532,9 +538,9 @@ function crit_z(c::Float64=0.95; twosided::Bool=true)
 end
 
 """
-    z2pow(z)
+    z2p(z)
 
-Calculate power for a given z value.
+Calculate probability for a given z value.
 
 # Arguments
 
@@ -542,33 +548,38 @@ Calculate power for a given z value.
 
 # Returns
 
-- `power::Float64`
+- `p::Float64`
 """
-function z2pow(z::Real)
+function z2p(z::Real)
 
-    return cdf(Distributions.Normal(0.0, 1.0), z)
+    p = cdf(Distributions.Normal(0.0, 1.0), z)
+
+    return p
 
 end
 
 """
-    cmp_stat(stat_dist, stat_value)
+    cmp_stat(stat_dist, v)
 
 Calculate proportion of elements below or above a given statistic value.
 
 # Arguments
 
 - `stat_dist::AbstractVector`: statistic values distribution
-- `stat_value::Real`: statistic value
-- `type::Symbol=:g`: calculation proportion of elements greater (`:g`) or lesser (`:l`) than `stat_value`
+- `v::Real`: statistic value
+- `type::Symbol=:g`: calculation proportion of elements greater (`:g`) or lesser (`:l`) than `v`
 
 # Returns
 
 - `p::Float64`
 """
-function cmp_stat(stat_dist::AbstractVector, stat_value::Real; type::Symbol=:g)
+function cmp_stat(stat_dist::AbstractVector, v::Real; type::Symbol=:g)
+
     _check_var(type, [:g, :l], "type")
-    type === :g && return count(stat_dist .> stat_value) / length(stat_dist)
-    type === :l && return count(stat_dist .< stat_value) / length(stat_dist)
+
+    type === :g && return count(stat_dist .> v) / length(stat_dist)
+    type === :l && return count(stat_dist .< v) / length(stat_dist)
+
 end
 
 """
@@ -617,7 +628,7 @@ Calculate cosine similarity.
 """
 function cosine_similarity(s1::AbstractVector, s2::AbstractVector)
 
-    @assert length(s1) == length(s2) "Length of both vectors must be the same."
+    @assert length(s1) == length(s2) "Both vectors must have the same length."
 
     cs = sum(s1 .* s2) / (sqrt(sum(s1.^2)) * sqrt(sum(s2.^2)))
 
