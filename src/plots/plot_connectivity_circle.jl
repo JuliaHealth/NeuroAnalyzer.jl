812d0b4f3f193ad5a5ad75c4c986ab6a8712710b
Adam Wysokinski <adam.wysokinski@tutanota.com>
2025-04-05 09:16:49 +0200

    update: plot_connectivity_circle() [CI SKIP]

--- src/plots/plot_connectivity_circle.jl
+++ src/plots/plot_connectivity_circle.jl
@@ -39,7 +39,7 @@ function plot_connectivity_circle(m::AbstractMatrix; clabels=Vector{String}, tit
 
     @assert size(m, 1) == length(clabels) "Number of channels in m ($(size(m, 1))) and clabels length ($(length(clabels))) differ."
     @assert size(m, 1) >= 2 "m must contain data for ≥ 2 channels."
-    @assert size(m, 1) == size(m, 1) "m must be a square matrix."
+    @assert size(m, 1) == size(m, 2) "m must be a square matrix."
 
     t = linspace(-pi, pi, size(m, 1) + 1)
     pos_x = zeros(size(m, 1))
@@ -71,26 +71,31 @@ function plot_connectivity_circle(m::AbstractMatrix; clabels=Vector{String}, tit
 
     # draw connections
     m_norm = NeuroStats.normalize_minmax(m)
+    c = (0.0, 0.0)
     s = size(m, 1)
     for idx1 in 1:s
         for idx2 in (idx1 + 1):s
             if !isnothing(threshold)
-                threshold_type === :eq && m[idx1, idx2] != threshold && break
-                threshold_type === :neq && m[idx1, idx2] == threshold && break
-                threshold_type === :g && m[idx1, idx2] <= threshold && break
-                threshold_type === :l && m[idx1, idx2] >= threshold && break
-                threshold_type === :geq && m[idx1, idx2] < threshold && break
-                threshold_type === :leq && m[idx1, idx2] > threshold && break
+                (threshold_type === :eq && m[idx1, idx2] != threshold) && break
+                (threshold_type === :neq && m[idx1, idx2] == threshold) && break
+                (threshold_type === :g && m[idx1, idx2] <= threshold) && break
+                (threshold_type === :l && m[idx1, idx2] >= threshold) && break
+                (threshold_type === :geq && m[idx1, idx2] < threshold) && break
+                (threshold_type === :leq && m[idx1, idx2] > threshold) && break
             end
-            px = [pos_x[idx1], 0.0, pos_x[idx2]]
-            py = [pos_y[idx1], 0.0, pos_y[idx2]]
+            mid_x = (pos_x[idx1] + pos_x[idx2]) / 2
+            mid_y = (pos_y[idx1] + pos_y[idx2]) / 2
+            d = distance((pos_x[idx1], pos_y[idx1]), (pos_x[idx2], pos_y[idx2]))
+            c = (mid_x * (1 - d * 0.5), mid_y * (1 - d * 0.5))
+            px = [pos_x[idx1], c[1], pos_x[idx2]]
+            py = [pos_y[idx1], c[2], pos_y[idx2]]
             x_vals, y_vals = _bernstein_poly(px, py; steps=50)
-            c = :black
-            m[idx1, idx2] < 0 && (c = :blue)
-            m[idx1, idx2] > 0 && (c = :red)
+            col = :black
+            m[idx1, idx2] < 0 && (col = :blue)
+            m[idx1, idx2] > 0 && (col = :red)
             plot!(x_vals,
                   y_vals,
-                  color=c,
+                  color=col,
                   lw=10 * abs(m_norm[idx1, idx2]),
                   alpha=0.5,
                   label="")
