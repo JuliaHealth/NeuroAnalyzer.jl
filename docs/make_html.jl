623886e1d31b4a624d064a6ed40c11f7ea507568
Adam Wysokinski <adam.wysokinski@tutanota.com>
2025-08-02 01:33:41 +0200

    update: CI, docs

--- docs/make_html.jl
+++ docs/make_html.jl
@@ -1,35 +1,10 @@
 @info "Generating HTML documentation"
 
 using Documenter
-using NeuroAnalyzer
 
-#=
 using Pkg
-Pkg.add(["Documenter", "Plots", "DataFrames", "Wavelets", "ContinuousWavelets", "StatsModels", "MultivariateStats"])
 Pkg.activate("..")
-Pkg.add(url="https://codeberg.org/AdamWysokinski/FIRLSFilterDesign.jl")
-Pkg.instantiate()
-
-@info "Loading package: Documenter"
-using Documenter
-@info "Loading package: Plots"
-using Plots
-@info "Loading package: Cairo"
-using Cairo
-@info "Loading package: DataFrames"
-using DataFrames
-@info "Loading package: Wavelets"
-using Wavelets
-@info "Loading package: ContinuousWavelets"
-using ContinuousWavelets
-@info "Loading package: StatsModels"
-using StatsModels
-@info "Loading package: MultivariateStats"
-using MultivariateStats
-@info "Loading package: NeuroAnalyzer"
 using NeuroAnalyzer
-println()
-=#
 
 makedocs(sitename="NeuroAnalyzer.jl",
          modules=[NeuroAnalyzer],
