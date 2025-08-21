255ad163081313da8b1eb10a4521942397e46ef5
Adam Wysokinski <adam.wysokinski@tutanota.com>
2025-08-22 00:53:12 +0200

    update: hmspectrum()

--- src/analyze/hmspectrum.jl
+++ src/analyze/hmspectrum.jl
@@ -5,8 +5,10 @@ Hilbert marginal spectrum
 
 """
 function hmspectrum(obj; ch)
+
     p, _, t = NeuroAnalyzer.spectrogram(obj, ch=ch, method=:hht, db=false)
     p = dropdims(sum(p, dims=1), dims=3)
-    # Plots.plot(t, p[1, :, 1])
+
     return p, t
+
 end
