64e63c4ed35b788ac29b9b88b978b2ef5a75aace
Adam Wysokinski <adam.wysokinski@tutanota.com>
2025-08-22 22:26:41 +0200

    update: hmspectrum()

--- src/analyze/hmspectrum.jl
+++ src/analyze/hmspectrum.jl
@@ -1,14 +1,26 @@
 export hmspectrum
 
 """
-Hilbert marginal spectrum
+    hmspectrum(obj; ch)
 
+Calculate Hilbert marginal spectrum.
+
+# Arguments
+
+- `obj::NeuroAnalyzer.NEURO`
+- `ch::String`: channel name
+
+# Returns
+
+Named tuple containing:
+- `p::Array{Float64, 3}`: Hilbert marginal spectra for each epoch
+- `t::Vector{Float64}`: time points
 """
-function hmspectrum(obj; ch)
+function hmspectrum(obj; ch::String)::@NamedTuple{p::Array{Float64, 3}, t::Vector{Float64}}
 
     p, _, t = NeuroAnalyzer.spectrogram(obj, ch=ch, method=:hht, db=false)
     p = dropdims(sum(p, dims=1), dims=3)
 
-    return p, t
+    return (p=p, t=t)
 
 end
