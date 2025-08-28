b101f64ad131a36c67417c51b880c2651ff8016e
Adam Wysokinski <adam.wysokinski@tutanota.com>
2025-08-28 18:24:21 +0200

    update: plot_save()

--- src/plots/plot_save.jl
+++ src/plots/plot_save.jl
@@ -7,21 +7,25 @@ Saves plot as file (PNG/PDF). File format is determined using `file_name` extens
 
 # Arguments
 
-- `p::Plots.Plot{Plots.GRBackend}`
+- `p::Union{Plots.Plot{Plots.GRBackend}, Makie.Figure}`
 - `file_name::String`
 
 # Returns
 
 Nothing
 """
-function plot_save(p::Plots.Plot{Plots.GRBackend}; file_name::String)::Nothing
+function plot_save(p::Union{Plots.Plot{Plots.GRBackend}, Makie.Figure}; file_name::String)::Nothing
 
     ext = splitext(file_name)[2]
     _check_var(ext, [".png", ".pdf"], "File format")
 
     (isfile(file_name) && verbose) && _warn("File $file_name will be overwritten.")
     try
-        Plots.savefig(p, file_name)
+        if p isa Plots.Plot{Plots.GRBackend}
+            Plots.savefig(p, file_name)
+        else
+            GLMakie.save(file_name, p)
+        end
     catch err
         if isa(err, SystemError)
             @error "File $file_name cannot be written."
