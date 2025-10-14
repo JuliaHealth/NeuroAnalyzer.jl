42499953d2b7211ed88588098ed89a0aae379e05
Adam Wysokinski <adam.wysokinski@tutanota.com>
2025-10-14 23:46:24 +0200

    update: coherence()

--- src/analyze/coherence.jl
+++ src/analyze/coherence.jl
@@ -24,12 +24,12 @@ Calculate coherence and MSC (magnitude-squared coherence).
 # Returns
 
 Named tuple containing:
-- `coh::Vector{Float64}`: coherence
-- `imcoh::Vector{ComplexF64}`: imaginary component of coherence
-- `mscoh::Vector{Float64}`: magnitude-squared coherence
+- `coh::Vector{ComplexF64}`: coherence
+- `imcoh::Vector{Float64}`: imaginary part of coherence
+- `msc::Vector{Float64}`: magnitude-squared coherence
 - `f::Vector{Float64}`: frequencies
 """
-function coherence(s1::AbstractVector, s2::AbstractVector; method::Symbol=:mt, fs::Int64, frq_lim::Tuple{Real, Real}=(0, fs / 2), demean::Bool=false, nt::Int64=7, wlen::Int64=fs, woverlap::Int64=round(Int64, wlen * 0.97), w::Bool=true)::@NamedTuple{coh::Vector{Float64}, imcoh::Vector{ComplexF64}, mscoh::Vector{Float64}, f::Vector{Float64}}
+function coherence(s1::AbstractVector, s2::AbstractVector; method::Symbol=:mt, fs::Int64, frq_lim::Tuple{Real, Real}=(0, fs / 2), demean::Bool=false, nt::Int64=7, wlen::Int64=fs, woverlap::Int64=round(Int64, wlen * 0.97), w::Bool=true)::@NamedTuple{coh::Vector{ComplexF64}, imcoh::Vector{Float64}, msc::Vector{Float64}, f::Vector{Float64}}
 
     _check_var(method, [:mt, :fft, :stft], "method")
     s1, s2 = _veqlen(s1, s2)
@@ -44,10 +44,12 @@ function coherence(s1::AbstractVector, s2::AbstractVector; method::Symbol=:mt, f
     s1s1, f = cpsd(s1, s1, fs=fs, wlen=wlen, woverlap=woverlap, w=w, demean=demean, method=method, frq_lim=frq_lim)
     s1s2, f = cpsd(s1, s2, fs=fs, wlen=wlen, woverlap=woverlap, w=w, demean=demean, method=method, frq_lim=frq_lim)
     s2s2, f = cpsd(s2, s2, fs=fs, wlen=wlen, woverlap=woverlap, w=w, demean=demean, method=method, frq_lim=frq_lim)
-    coh = @. abs(s1s2) / sqrt(s1s1 * s2s2)
-    imcoh = @. imag(s1s2) / sqrt(s1s1 * s2s2)
-    mscoh = @. (abs(s1s2))^2 / (s1s1 * s2s2)
-    return (coh=coh, imcoh=imcoh, mscoh=mscoh, f=f)
+
+    coh = @. s1s2 / sqrt(s1s1 * s2s2)
+    imcoh = @. imag(coh)
+    msc = @. abs(coh)^2
+
+    return (coh=coh, imcoh=imcoh, msc=msc, f=f)
 
 #=
     if method === :mt
@@ -62,8 +64,8 @@ function coherence(s1::AbstractVector, s2::AbstractVector; method::Symbol=:mt, f
         f2_idx = vsearch(frq_lim[2], f)
         f = f[f1_idx:f2_idx]
         coh = @views coh[1, 2, f1_idx:f2_idx]
-        mscoh = coh.^2
-        return (coh=coh, mscoh=mscoh, f=f)
+        msc = coh.^2
+        return (coh=coh, msc=msc, f=f)
     elseif method in [:fft, :stft]
     end
 =#
@@ -93,12 +95,12 @@ Calculate coherence and MSC (magnitude-squared coherence).
 
 # Returns
 
-- `coh::Array{Float64, 3}`: coherence
-- `coh::Array{Float64, 3}`: coherence
-- `mscoh::Array{Float64, 3}`: magnitude-squared coherence
+- `coh::Array{ComplexF64, 3}`: coherence
+- `imcoh::Array{Float64, 3}`: imaginary part of coherence
+- `msc::Array{Float64, 3}`: magnitude-squared coherence
 - `f::Vector{Float64}`: frequencies
 """
-function coherence(s1::AbstractArray, s2::AbstractArray; method::Symbol=:mt, fs::Int64, frq_lim::Tuple{Real, Real}=(0, fs / 2), demean::Bool=false, nt::Int64=7, wlen::Int64=fs, woverlap::Int64=round(Int64, wlen * 0.97), w::Bool=true)::@NamedTuple{coh::Array{Float64, 3}, imcoh::Array{Float64, 3}, mscoh::Array{Float64, 3}, f::Vector{Float64}}
+function coherence(s1::AbstractArray, s2::AbstractArray; method::Symbol=:mt, fs::Int64, frq_lim::Tuple{Real, Real}=(0, fs / 2), demean::Bool=false, nt::Int64=7, wlen::Int64=fs, woverlap::Int64=round(Int64, wlen * 0.97), w::Bool=true)::@NamedTuple{coh::Array{ComplexF64, 3}, imcoh::Array{Float64, 3}, msc::Array{Float64, 3}, f::Vector{Float64}}
 
     @assert size(s1) == size(s2) "s1 and s2 must have the same size."
 
@@ -107,7 +109,7 @@ function coherence(s1::AbstractArray, s2::AbstractArray; method::Symbol=:mt, fs:
     ch_n = size(s1, 1)
     ep_n = size(s1, 3)
 
-    _, f = NeuroAnalyzer.coherence(s1[1, :, 1], s2[1, :, 1]; method=method, fs=fs, frq_lim=frq_lim, demean=demean, nt=nt, wlen=wlen, woverlap=woverlap, w=w)
+    _, _, _, f = NeuroAnalyzer.coherence(s1[1, :, 1], s2[1, :, 1]; method=method, fs=fs, frq_lim=frq_lim, demean=demean, nt=nt, wlen=wlen, woverlap=woverlap, w=w)
     if frq_lim !== nothing
         _check_tuple(frq_lim, "frq_lim", (0, fs / 2))
         idx1 = vsearch(frq_lim[1], f)
@@ -115,16 +117,17 @@ function coherence(s1::AbstractArray, s2::AbstractArray; method::Symbol=:mt, fs:
         f = f[idx1:idx2]
     end
 
-    coh = zeros(ch_n, length(f), ep_n)
-    mscoh = zeros(ch_n, length(f), ep_n)
+    coh = zeros(ComplexF64, ch_n, length(f), ep_n)
+    imcoh = zeros(ch_n, length(f), ep_n)
+    msc = zeros(ch_n, length(f), ep_n)
 
     @inbounds for ep_idx in 1:ep_n
         Threads.@threads for ch_idx in 1:ch_n
-            coh[ch_idx, :, ep_idx], mscoh[ch_idx, :, ep_idx], _ = @views NeuroAnalyzer.coherence(s1[ch_idx, :, ep_idx], s2[ch_idx, :, ep_idx], method=method, fs=fs, frq_lim=frq_lim, demean=demean, nt=nt, wlen=wlen, woverlap=woverlap, w=w)
+            coh[ch_idx, :, ep_idx], imcoh[ch_idx, :, ep_idx], imcoh[ch_idx, :, ep_idx], _ = @views NeuroAnalyzer.coherence(s1[ch_idx, :, ep_idx], s2[ch_idx, :, ep_idx], method=method, fs=fs, frq_lim=frq_lim, demean=demean, nt=nt, wlen=wlen, woverlap=woverlap, w=w)
         end
     end
 
-    return (coh=coh, mscoh=mscoh, f=f)
+    return (coh=coh, imcoh=imcoh, msc=msc, f=f)
 end
 
 """
@@ -154,12 +157,12 @@ Calculate coherence and MSC (magnitude-squared coherence).
 
 # Returns
 
-- `coh::Array{Float64, 3}`: coherence
-- `imcoh::Array{ComplexF64, 3}`: imaginary component of coherence
-- `mscoh::Array{Float64, 3}`: magnitude-squared coherence
+- `coh::Array{ComplexF64, 3}`: coherence
+- `imcoh::Array{Float64, 3}`: imaginary part of coherence
+- `msc::Array{Float64, 3}`: magnitude-squared coherence
 - `f::Vector{Float64}`: frequencies
 """
-function coherence(obj1::NeuroAnalyzer.NEURO, obj2::NeuroAnalyzer.NEURO; ch1::Union{String, Vector{String}}, ch2::Union{String, Vector{String}}, ep1::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj1)), ep2::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj2)), method::Symbol=:mt, frq_lim::Tuple{Real, Real}=(0, sr(obj1) / 2), demean::Bool=false, nt::Int64=7, wlen::Int64=sr(obj1), woverlap::Int64=round(Int64, wlen * 0.97), w::Bool=true)::@NamedTuple{coh::Array{Float64, 3}, imcoh::Array{ComplexF64, 3}, mscoh::Array{Float64, 3}, f::Vector{Float64}}
+function coherence(obj1::NeuroAnalyzer.NEURO, obj2::NeuroAnalyzer.NEURO; ch1::Union{String, Vector{String}}, ch2::Union{String, Vector{String}}, ep1::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj1)), ep2::Union{Int64, Vector{Int64}, AbstractRange}=_c(nepochs(obj2)), method::Symbol=:mt, frq_lim::Tuple{Real, Real}=(0, sr(obj1) / 2), demean::Bool=false, nt::Int64=7, wlen::Int64=sr(obj1), woverlap::Int64=round(Int64, wlen * 0.97), w::Bool=true)::@NamedTuple{coh::Array{ComplexF64, 3}, imcoh::Array{Float64, 3}, msc::Array{Float64, 3}, f::Vector{Float64}}
 
     @assert sr(obj1) == sr(obj2) "OBJ1 and OBJ2 must have the same sampling rate."
     @assert length(ch1) == length(ch2) "Lengths of ch1 ($(length(ch1)) and ch2 ($(length(ch2)) must be equal."
@@ -173,8 +176,8 @@ function coherence(obj1::NeuroAnalyzer.NEURO, obj2::NeuroAnalyzer.NEURO; ch1::Un
     isa(ep1, Int64) && (ep1 = [ep1])
     isa(ep2, Int64) && (ep2 = [ep2])
 
-    coh, imcoh, mscoh, f = @views NeuroAnalyzer.coherence(obj1.data[ch1, :, ep1], obj2.data[ch2, :, ep2], method=method, fs=sr(obj1), frq_lim=frq_lim, demean=demean, nt=nt, wlen=wlen, woverlap=woverlap, w=w)
+    coh, imcoh, msc, f = @views NeuroAnalyzer.coherence(obj1.data[ch1, :, ep1], obj2.data[ch2, :, ep2], method=method, fs=sr(obj1), frq_lim=frq_lim, demean=demean, nt=nt, wlen=wlen, woverlap=woverlap, w=w)
 
-    return (coh=coh, imcoh=imcoh, mscoh=mscoh, f=f)
+    return (coh=coh, imcoh=imcoh, msc=msc, f=f)
 
 end
